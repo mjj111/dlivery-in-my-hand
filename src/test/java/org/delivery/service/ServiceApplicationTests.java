18322f945f148ac0a50a140a5f6dfa7071dbc60d
김명준 <86913355+mjj111@users.noreply.github.com>
2023-07-10 22:17:39 +0900

    BUILD 자바 11 , Springboot 2.7.9 기반의 프로젝트 생성
    #1

---
+++ src/test/java/org/delivery/service/ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.delivery.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
